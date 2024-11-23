4e2055624c3cd2708ed4195e5b8c9250d96d568e
Alessandro Rossi <4215912+kubealex@users.noreply.github.com>
2024-11-23 19:13:41 +0100

    Update load_application_data.sh

--- files/load_application_data.sh
+++ files/load_application_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mysql -uroot -e "CREATE DATABASE petclinic"
 sudo mysql -uroot petclinic < /opt/petclinic.sql
-sudo rm -rf /home/bootc-user/load_application_data.sh
+sudo rm -rf /home/bootc-user/load_data
 sudo systemctl disable --now load-data
 sudo systemctl restart petclinic
